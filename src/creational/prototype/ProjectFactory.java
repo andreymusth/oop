ee6db9792fc4a9c01ff52ccad9d2f70e2ef3be43
Andrey Belous <andreyrabo@mail.ru>
2018-12-17 09:00:47 +0300

    Prototype has been added

---
+++ src/creational/prototype/ProjectFactory.java
@@ -0,0 +1,19 @@
+package creational.prototype;
+
+public class ProjectFactory {
+
+
+    private Project project;
+
+    public ProjectFactory(Project project) {
+        this.project = project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    Project cloneProject() {
+        return (Project) project.copy();
+    }
+}
